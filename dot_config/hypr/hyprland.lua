c84b0a3532819c54d59ec4a87239783ec840f96c
David Meents <dmeents@pm.me>
2026-09-13 11:55:15 -0700

    Add .config/hypr/config/animations.lua Add .config/hypr/config/colors.lua Add .config/hypr/config/decorations.lua Add .config/hypr/config/environment.lua Add .config/hypr/config/input.lua Add .config/hypr/config/misc.lua Add .config/hypr/hyprland.lua

---
+++ dot_config/hypr/hyprland.lua
@@ -0,0 +1,16 @@
+-- CachyOS Hyprland Configuration
+
+require("config.animations")
+require("config.autostart")
+require("config.colors")
+require("config.decorations")
+require("config.defaults")
+require("config.environment")
+require("config.input")
+require("config.keybinds")
+require("config.misc")
+require("config.monitors")
+require("config.windowrules")
+
+-- For Noctalia Color templates
+require("noctalia").apply_theme()
